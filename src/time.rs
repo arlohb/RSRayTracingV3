336fc8eab8e585cee6be26968d6c38441b2e1d9f
arlohb <arlo@urwick.co.uk>
2022-04-04 18:50:10 +0100

    Changed an unwrap to an expect

--- src/time.rs
+++ src/time.rs
@@ -5,7 +5,8 @@ pub struct Time {}
 impl Time {
   pub fn now_millis() -> f64 {
     SystemTime::now()
-      .duration_since(SystemTime::UNIX_EPOCH).unwrap()
+      .duration_since(SystemTime::UNIX_EPOCH)
+      .expect("Time went backwards")
       .as_micros() as f64 / 1000.
   }
 }
