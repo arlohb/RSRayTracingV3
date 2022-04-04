68f8fd97b277cb6c9a5ea7b2b92a0d7e6d3d071e
arlohb <arlo@urwick.co.uk>
2022-04-04 22:27:24 +0100

    Cleaned up movement.rs

--- src/movement.rs
+++ src/movement.rs
@@ -1,58 +1,34 @@
-use eframe::egui;
+use eframe::egui::{InputState, Key};
 
 use crate::ray_tracer::Camera;
 
 pub fn move_and_rotate(
-  input: &egui::InputState,
+  input: &InputState,
   camera: &mut Camera,
   look_speed: f64,
   move_speed: f64,
   shift_mod: f64,
   ctrl_mod: f64,
 ) {
-  if input.key_down(egui::Key::ArrowRight) {
-    camera.rotation.y += look_speed;
-  }
-  if input.key_down(egui::Key::ArrowLeft) {
-    camera.rotation.y -= look_speed;
-  }
-  if input.key_down(egui::Key::ArrowUp) {
-    camera.rotation.x -= look_speed;
-  }
-  if input.key_down(egui::Key::ArrowDown) {
-    camera.rotation.x += look_speed;
-  }
+  if input.key_down(Key::ArrowRight) { camera.rotation.y += look_speed; }
+  if input.key_down(Key::ArrowLeft) { camera.rotation.y -= look_speed; }
+  if input.key_down(Key::ArrowUp) { camera.rotation.x -= look_speed; }
+  if input.key_down(Key::ArrowDown) { camera.rotation.x += look_speed; }
 
   camera.clamp_rotation();
 
   let move_speed = if input.modifiers.shift {
     move_speed * shift_mod
-  } else {
-    move_speed
-  };
-
-  let move_speed = if input.modifiers.ctrl {
+  } else if input.modifiers.ctrl {
     move_speed * ctrl_mod
   } else {
     move_speed
   };
 
-  if input.key_down(egui::Key::W) {
-    camera.position -= camera.forward() * move_speed;
-  }
-  if input.key_down(egui::Key::S) {
-    camera.position += camera.forward() * move_speed;
-  }
-  if input.key_down(egui::Key::D) {
-    camera.position += camera.right() * move_speed;
-  }
-  if input.key_down(egui::Key::A) {
-    camera.position -= camera.right() * move_speed;
-  }
-  if input.key_down(egui::Key::E) {
-    camera.position += camera.up() * move_speed;
-  }
-  if input.key_down(egui::Key::Q) {
-    camera.position -= camera.up() * move_speed;
-  }
+  if input.key_down(Key::W) { camera.position -= camera.forward() * move_speed; }
+  if input.key_down(Key::S) { camera.position += camera.forward() * move_speed; }
+  if input.key_down(Key::D) { camera.position += camera.right() * move_speed; }
+  if input.key_down(Key::A) { camera.position -= camera.right() * move_speed; }
+  if input.key_down(Key::E) { camera.position += camera.up() * move_speed; }
+  if input.key_down(Key::Q) { camera.position -= camera.up() * move_speed; }
 }
