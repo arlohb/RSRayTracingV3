b7252efe084c3cd3771d874af273c1f290e5e251
arlohb <arlo@urwick.co.uk>
2022-04-04 22:10:48 +0100

    Rotation is now clamped in the camera struct

--- src/ray_tracer/camera.rs
+++ src/ray_tracer/camera.rs
@@ -4,11 +4,24 @@ use crate::ray_tracer::*;
 #[derive(Deserialize, Serialize, Clone)]
 pub struct Camera {
   pub position: Vec3,
+  // in radians
   pub rotation: Vec3,
   pub fov: f64,
 }
 
 impl Camera {
+  pub fn clamp_rotation(&mut self) {
+    // x should be clamped between -pi/2 and pi/2
+    self.rotation.x = self.rotation.x.clamp(-0.5 * std::f64::consts::PI, 0.5 * std::f64::consts::PI);
+
+    // y should be wrapped around to between -pi and pi
+    if self.rotation.y < std::f64::consts::PI { self.rotation.y += 2. * std::f64::consts::PI; }
+    if self.rotation.y > std::f64::consts::PI { self.rotation.y -= 2. * std::f64::consts::PI; }
+
+    // z should be clamped between -pi and pi
+    self.rotation.z = self.rotation.z.clamp(-std::f64::consts::PI, std::f64::consts::PI);
+  }
+
   pub fn forward(&self) -> Vec3 {
     Vec3 { x: 0., y: 0., z: 1. }
       .transform_point(Mat44::create_rotation(Axis::X, -self.rotation.x))
