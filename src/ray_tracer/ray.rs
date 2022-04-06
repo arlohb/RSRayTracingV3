2f4861bd85c60e7b1fb643e5410b823c5d192947
arlohb <arlo@urwick.co.uk>
2022-04-06 14:23:37 +0100

    Documented ray.rs

--- src/ray_tracer/ray.rs
+++ src/ray_tracer/ray.rs
@@ -1,21 +1,28 @@
 use crate::ray_tracer::*;
 
-pub struct Hit<'a> {
+/// Represents an intersection between a ray and an object.
+struct Hit<'a> {
+  /// The distance along a ray at which the intersection is.
   pub distance: f64,
+  /// The intersection point.
   pub point: Vec3,
+  /// The object that was hit.
   pub object: &'a Object,
 }
 
+/// Represents a ray.
 pub struct Ray {
   pub origin: Vec3,
   pub direction: Vec3,
 }
 
 impl Ray {
+  /// Return the direction of a ray reflected off a surface with the given normal.
   pub fn get_reflection_vec(ray_dir: Vec3, surface_normal: Vec3) -> Vec3 {
     (surface_normal * surface_normal.dot(ray_dir)) * 2. - ray_dir
   }
 
+  /// Return the object and intersection point that the ray hits first.
   pub fn closest_intersection<'a>(&self, scene: &'a Scene) -> Option<(&'a Object, Vec3)> {
     let mut hit: Option<Hit> = None;
 
