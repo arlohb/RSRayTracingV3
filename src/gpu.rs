73d651be4c3f30668b5daec6923e8167f03977f8
arlohb <arlo@urwick.co.uk>
2022-04-12 19:32:55 +0100

    Consistent tab size

--- src/gpu.rs
+++ src/gpu.rs
@@ -1,282 +1,283 @@
 use std::{
-    borrow::Cow,
-    sync::{Arc, Mutex},
+  borrow::Cow,
+  sync::{Arc, Mutex},
 };
 use wgpu::util::DeviceExt;
 use winit::{
-    event::{Event, WindowEvent},
-    event_loop::{ControlFlow, EventLoop},
-    window::Window,
+  event::{Event, WindowEvent},
+  event_loop::{ControlFlow, EventLoop},
+  window::Window,
 };
 
 use crate::ray_tracer::Scene;
 
 fn parse_shader(bounce_limit: u32) -> String {
-    let mut dict = std::collections::HashMap::<&str, String>::new();
-    dict.insert("bounce_limit", format!("{}u", bounce_limit));
-    dict.insert("metallic_stack_values", "0., ".repeat(bounce_limit as usize));
-    dict.insert("reflection_colour_stack_values", "vec3<f32>(0., 0., 0.), ".repeat(bounce_limit as usize));
+  let mut dict = std::collections::HashMap::<&str, String>::new();
+  dict.insert("bounce_limit", format!("{}u", bounce_limit));
+  dict.insert("metallic_stack_values", "0., ".repeat(bounce_limit as usize));
+  dict.insert("reflection_colour_stack_values", "vec3<f32>(0., 0., 0.), ".repeat(bounce_limit as usize));
 
-    let vert = include_str!("vert.wgsl").to_string();
+  let vert = include_str!("vert.wgsl").to_string();
 
-    let frag = include_str!("frag.wgsl").to_string();
+  let frag = include_str!("frag.wgsl").to_string();
 
-    let frag_parsed = frag.lines()
-        .filter(|line| !line.contains("////"))
-        .map(|line| {
-            let split = line.split("//").collect::<Vec<_>>();
-            if split.len() == 3 {
-                let template_name = split[1].trim();
-                let template_value = dict[template_name].as_str();
-                split[0].to_string() + template_value + split[2]
-            } else {
-                line.to_string()
-            }
-        })
-        .collect::<Vec<_>>()
-        .join("\n");
-    
-    vert + frag_parsed.as_str()
+  let frag_parsed = frag.lines()
+    .filter(|line| !line.contains("////"))
+    .map(|line| {
+      let split = line.split("//").collect::<Vec<_>>();
+      if split.len() == 3 {
+        let template_name = split[1].trim();
+        let template_value = dict[template_name].as_str();
+        split[0].to_string() + template_value + split[2]
+      } else {
+        line.to_string()
+      }
+    })
+    .collect::<Vec<_>>()
+    .join("\n");
+  
+  vert + frag_parsed.as_str()
 }
 
 pub async fn run(
-    event_loop: EventLoop<()>,
-    window: Window,
-    scene: Arc<Mutex<Scene>>,
-    frame_times: Arc<Mutex<crate::History>>,
-    fps_limit: f64,
+  event_loop: EventLoop<()>,
+  window: Window,
+  scene: Arc<Mutex<Scene>>,
+  frame_times: Arc<Mutex<crate::History>>,
+  fps_limit: f64,
 ) {
-    let size = window.inner_size();
-    let instance = wgpu::Instance::new(wgpu::Backends::all());
-    let surface = unsafe { instance.create_surface(&window) };
-    let adapter = instance
-        .request_adapter(&wgpu::RequestAdapterOptions {
-            power_preference: wgpu::PowerPreference::default(),
-            force_fallback_adapter: false,
-            // Request an adapter which can render to our surface
-            compatible_surface: Some(&surface),
-        })
-        .await
-        .expect("Failed to find an appropriate adapter");
+  let size = window.inner_size();
+  let instance = wgpu::Instance::new(wgpu::Backends::all());
+  let surface = unsafe { instance.create_surface(&window) };
+  let adapter = instance
+    .request_adapter(&wgpu::RequestAdapterOptions {
+      power_preference: wgpu::PowerPreference::default(),
+      force_fallback_adapter: false,
+      // Request an adapter which can render to our surface
+      compatible_surface: Some(&surface),
+    })
+    .await
+    .expect("Failed to find an appropriate adapter");
 
-    // Create the logical device and command queue
-    let (device, queue) = adapter
-        .request_device(
-            &wgpu::DeviceDescriptor {
-                label: None,
-                features: wgpu::Features::BUFFER_BINDING_ARRAY
-                    | wgpu::Features::STORAGE_RESOURCE_BINDING_ARRAY,
-                limits: wgpu::Limits::default(),
-            },
-            None,
-        )
-        .await
-        .expect("Failed to create device");
-
-    // Load the shaders from disk
-    let shader = device.create_shader_module(&wgpu::ShaderModuleDescriptor {
+  // Create the logical device and command queue
+  let (device, queue) = adapter
+    .request_device(
+      &wgpu::DeviceDescriptor {
         label: None,
-        source: wgpu::ShaderSource::Wgsl(Cow::Borrowed(
-            parse_shader(scene.lock().unwrap().reflection_limit).as_str(),
-        )),
-    });
+        features: wgpu::Features::BUFFER_BINDING_ARRAY
+          | wgpu::Features::STORAGE_RESOURCE_BINDING_ARRAY,
+        limits: wgpu::Limits::default(),
+      },
+      None,
+    )
+    .await
+    .expect("Failed to create device");
 
-    let bind_group_layout = device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
-        label: None,
-        entries: &[
-            wgpu::BindGroupLayoutEntry {
-                binding: 0,
-                visibility: wgpu::ShaderStages::FRAGMENT,
-                ty: wgpu::BindingType::Buffer {
-                    ty: wgpu::BufferBindingType::Storage { read_only: true },
-                    has_dynamic_offset: false,
-                    min_binding_size: None,
-                },
-                count: None,
-            },
-            wgpu::BindGroupLayoutEntry {
-                binding: 1,
-                visibility: wgpu::ShaderStages::FRAGMENT,
-                ty: wgpu::BindingType::Buffer {
-                    ty: wgpu::BufferBindingType::Storage { read_only: true },
-                    has_dynamic_offset: false,
-                    min_binding_size: None,
-                },
-                count: None,
-            },
-            wgpu::BindGroupLayoutEntry {
-                binding: 2,
-                visibility: wgpu::ShaderStages::FRAGMENT,
-                ty: wgpu::BindingType::Buffer {
-                    ty: wgpu::BufferBindingType::Storage { read_only: true },
-                    has_dynamic_offset: false,
-                    min_binding_size: None,
-                },
-                count: None,
-            },
-        ],
-    });
+  // Load the shaders from disk
+  let shader = device.create_shader_module(&wgpu::ShaderModuleDescriptor {
+    label: None,
+    source: wgpu::ShaderSource::Wgsl(Cow::Borrowed(
+      parse_shader(scene.lock().unwrap().reflection_limit).as_str(),
+    )),
+  });
 
-    let width = window.inner_size().width as u32;
-    let height = window.inner_size().height as u32;
-    let (object_bytes, light_bytes, config_bytes) = scene.lock().unwrap().as_bytes(width, height);
-
-    let object_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
-        label: None,
-        contents: &object_bytes,
-        usage: wgpu::BufferUsages::STORAGE
-            | wgpu::BufferUsages::COPY_DST
-            | wgpu::BufferUsages::COPY_SRC,
-    });
-    let light_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
-        label: None,
-        contents: &light_bytes,
-        usage: wgpu::BufferUsages::STORAGE
-            | wgpu::BufferUsages::COPY_DST
-            | wgpu::BufferUsages::COPY_SRC,
-    });
-    let config_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
-        label: None,
-        contents: &config_bytes,
-        usage: wgpu::BufferUsages::STORAGE
-            | wgpu::BufferUsages::COPY_DST
-            | wgpu::BufferUsages::COPY_SRC,
-    });
-
-    let bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
-        label: None,
-        layout: &bind_group_layout,
-        entries: &[
-            wgpu::BindGroupEntry {
-                binding: 0,
-                resource: object_buffer.as_entire_binding(),
-            },
-            wgpu::BindGroupEntry {
-                binding: 1,
-                resource: light_buffer.as_entire_binding(),
-            },
-            wgpu::BindGroupEntry {
-                binding: 2,
-                resource: config_buffer.as_entire_binding(),
-            },
-        ],
-    });
-
-    let pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
-        label: None,
-        bind_group_layouts: &[&bind_group_layout],
-        push_constant_ranges: &[],
-    });
-
-    let swapchain_format = surface.get_preferred_format(&adapter).unwrap();
-
-    let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
-        label: None,
-        layout: Some(&pipeline_layout),
-        vertex: wgpu::VertexState {
-            module: &shader,
-            entry_point: "vs_main",
-            buffers: &[],
+  let bind_group_layout = device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+    label: None,
+    entries: &[
+      wgpu::BindGroupLayoutEntry {
+        binding: 0,
+        visibility: wgpu::ShaderStages::FRAGMENT,
+        ty: wgpu::BindingType::Buffer {
+          ty: wgpu::BufferBindingType::Storage { read_only: true },
+          has_dynamic_offset: false,
+          min_binding_size: None,
         },
-        fragment: Some(wgpu::FragmentState {
-            module: &shader,
-            entry_point: "fs_main",
-            targets: &[swapchain_format.into()],
-        }),
-        primitive: wgpu::PrimitiveState::default(),
-        depth_stencil: None,
-        multisample: wgpu::MultisampleState::default(),
-        multiview: None,
-    });
+        count: None,
+      },
+      wgpu::BindGroupLayoutEntry {
+        binding: 1,
+        visibility: wgpu::ShaderStages::FRAGMENT,
+        ty: wgpu::BindingType::Buffer {
+          ty: wgpu::BufferBindingType::Storage { read_only: true },
+          has_dynamic_offset: false,
+          min_binding_size: None,
+        },
+        count: None,
+      },
+      wgpu::BindGroupLayoutEntry {
+        binding: 2,
+        visibility: wgpu::ShaderStages::FRAGMENT,
+        ty: wgpu::BindingType::Buffer {
+          ty: wgpu::BufferBindingType::Storage { read_only: true },
+          has_dynamic_offset: false,
+          min_binding_size: None,
+        },
+        count: None,
+      },
+    ],
+  });
 
-    let mut config = wgpu::SurfaceConfiguration {
-        usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
-        format: swapchain_format,
-        width: size.width,
-        height: size.height,
-        present_mode: wgpu::PresentMode::Mailbox,
-    };
+  let width = window.inner_size().width as u32;
+  let height = window.inner_size().height as u32;
+  let (object_bytes, light_bytes, config_bytes) = scene.lock().unwrap().as_bytes(width, height);
 
-    surface.configure(&device, &config);
+  let object_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+    label: None,
+    contents: &object_bytes,
+    usage: wgpu::BufferUsages::STORAGE
+      | wgpu::BufferUsages::COPY_DST
+      | wgpu::BufferUsages::COPY_SRC,
+  });
+  let light_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+    label: None,
+    contents: &light_bytes,
+    usage: wgpu::BufferUsages::STORAGE
+      | wgpu::BufferUsages::COPY_DST
+      | wgpu::BufferUsages::COPY_SRC,
+  });
+  let config_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+    label: None,
+    contents: &config_bytes,
+    usage: wgpu::BufferUsages::STORAGE
+      | wgpu::BufferUsages::COPY_DST
+      | wgpu::BufferUsages::COPY_SRC,
+  });
 
-    let mut last_time = crate::Time::now_millis();
+  let bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+    label: None,
+    layout: &bind_group_layout,
+    entries: &[
+      wgpu::BindGroupEntry {
+        binding: 0,
+        resource: object_buffer.as_entire_binding(),
+      },
+      wgpu::BindGroupEntry {
+        binding: 1,
+        resource: light_buffer.as_entire_binding(),
+      },
+      wgpu::BindGroupEntry {
+        binding: 2,
+        resource: config_buffer.as_entire_binding(),
+      },
+    ],
+  });
 
-    event_loop.run(move |event, _, control_flow| {
-        // Have the closure take ownership of the resources.
-        // `event_loop.run` never returns, therefore we must do this to ensure
-        // the resources are properly cleaned up.
-        let _ = (&instance, &adapter, &shader, &pipeline_layout);
+  let pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+    label: None,
+    bind_group_layouts: &[&bind_group_layout],
+    push_constant_ranges: &[],
+  });
 
-        *control_flow = ControlFlow::Poll;
-        match event {
-            Event::WindowEvent {
-                event: WindowEvent::Resized(size),
-                ..
-            } => {
-                // Reconfigure the surface with the new size
-                config.width = size.width;
-                config.height = size.height;
-                surface.configure(&device, &config);
-                // On macos the window needs to be redrawn manually after resizing
-                window.request_redraw();
-            }
-            Event::RedrawRequested(_) => {
-                let width = window.inner_size().width;
-                let height = window.inner_size().height;
-                let (object_bytes, light_bytes, config_bytes) =
-                    scene.lock().unwrap().as_bytes(width, height);
+  let swapchain_format = surface.get_preferred_format(&adapter).unwrap();
 
-                queue.write_buffer(&object_buffer, 0, object_bytes.as_slice());
-                queue.write_buffer(&light_buffer, 0, light_bytes.as_slice());
-                queue.write_buffer(&config_buffer, 0, config_bytes.as_slice());
+  let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+    label: None,
+    layout: Some(&pipeline_layout),
+    vertex: wgpu::VertexState {
+      module: &shader,
+      entry_point: "vs_main",
+      buffers: &[],
+    },
+    fragment: Some(wgpu::FragmentState {
+      module: &shader,
+      entry_point: "fs_main",
+      targets: &[swapchain_format.into()],
+    }),
+    primitive: wgpu::PrimitiveState::default(),
+    depth_stencil: None,
+    multisample: wgpu::MultisampleState::default(),
+    multiview: None,
+  });
 
-                let frame = surface
-                    .get_current_texture()
-                    .expect("Failed to acquire next swap chain texture");
-                let view = frame
-                    .texture
-                    .create_view(&wgpu::TextureViewDescriptor::default());
-                let mut encoder =
-                    device.create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
-                {
-                    let mut rpass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
-                        label: None,
-                        color_attachments: &[wgpu::RenderPassColorAttachment {
-                            view: &view,
-                            resolve_target: None,
-                            ops: wgpu::Operations {
-                                load: wgpu::LoadOp::Clear(wgpu::Color::GREEN),
-                                store: true,
-                            },
-                        }],
-                        depth_stencil_attachment: None,
-                    });
-                    rpass.set_pipeline(&render_pipeline);
-                    rpass.set_bind_group(0, &bind_group, &[]);
-                    rpass.draw(0..6, 0..1);
-                }
+  let mut config = wgpu::SurfaceConfiguration {
+    usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+    format: swapchain_format,
+    width: size.width,
+    height: size.height,
+    present_mode: wgpu::PresentMode::Mailbox,
+  };
 
-                queue.submit(Some(encoder.finish()));
-                frame.present();
-            }
-            Event::WindowEvent {
-                event: WindowEvent::CloseRequested,
-                ..
-            } => {
-              *control_flow = ControlFlow::Exit;
-            },
-            _ => {
-              if crate::Time::now_millis() - last_time > 1000. / fps_limit {
-                let now = crate::Time::now_millis();
-                let elapsed = now - last_time;
-                last_time = now;
-                if let Ok(mut frame_times) = frame_times.try_lock() {
-                    frame_times.add(elapsed);
-                }
+  surface.configure(&device, &config);
 
-                window.request_redraw();
-              }
-            }
+  let mut last_time = crate::Time::now_millis();
+
+  event_loop.run(move |event, _, control_flow| {
+    // Have the closure take ownership of the resources.
+    // `event_loop.run` never returns, therefore we must do this to ensure
+    // the resources are properly cleaned up.
+    let _ = (&instance, &adapter, &shader, &pipeline_layout);
+
+    *control_flow = ControlFlow::Poll;
+    match event {
+      Event::WindowEvent {
+        event: WindowEvent::Resized(size),
+        ..
+      } => {
+        // Reconfigure the surface with the new size
+        config.width = size.width;
+        config.height = size.height;
+        surface.configure(&device, &config);
+        // On macos the window needs to be redrawn manually after resizing
+        window.request_redraw();
+      }
+      Event::RedrawRequested(_) => {
+        let width = window.inner_size().width;
+        let height = window.inner_size().height;
+        let (object_bytes, light_bytes, config_bytes) =
+          scene.lock().unwrap().as_bytes(width, height);
+
+        queue.write_buffer(&object_buffer, 0, object_bytes.as_slice());
+        queue.write_buffer(&light_buffer, 0, light_bytes.as_slice());
+        queue.write_buffer(&config_buffer, 0, config_bytes.as_slice());
+
+        let frame = surface
+          .get_current_texture()
+          .expect("Failed to acquire next swap chain texture");
+        let view = frame
+          .texture
+          .create_view(&wgpu::TextureViewDescriptor::default());
+        let mut encoder =
+          device.create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
+
+        {
+          let mut rpass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+            label: None,
+            color_attachments: &[wgpu::RenderPassColorAttachment {
+              view: &view,
+              resolve_target: None,
+              ops: wgpu::Operations {
+                load: wgpu::LoadOp::Clear(wgpu::Color::GREEN),
+                store: true,
+              },
+            }],
+            depth_stencil_attachment: None,
+          });
+          rpass.set_pipeline(&render_pipeline);
+          rpass.set_bind_group(0, &bind_group, &[]);
+          rpass.draw(0..6, 0..1);
         }
-    });
+
+        queue.submit(Some(encoder.finish()));
+        frame.present();
+      }
+      Event::WindowEvent {
+        event: WindowEvent::CloseRequested,
+        ..
+      } => {
+        *control_flow = ControlFlow::Exit;
+      },
+      _ => {
+        if crate::Time::now_millis() - last_time > 1000. / fps_limit {
+          let now = crate::Time::now_millis();
+          let elapsed = now - last_time;
+          last_time = now;
+          if let Ok(mut frame_times) = frame_times.try_lock() {
+            frame_times.add(elapsed);
+          }
+
+          window.request_redraw();
+        }
+      }
+    }
+  });
 }
