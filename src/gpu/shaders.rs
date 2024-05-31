59a118cdbea9f396a489b31303e6943a5ad0a221
arlohb <arlo@urwick.co.uk>
2024-05-31 16:20:17 +0100

    Fixed shader compilation

--- src/gpu/shaders.rs
+++ src/gpu/shaders.rs
@@ -2,7 +2,15 @@ use inline_spirv::include_spirv;
 use std::borrow::Cow;
 
 pub fn vert_shader(device: &wgpu::Device) -> wgpu::ShaderModule {
-    let spirv = include_spirv!("src/shaders/vert.hlsl", vert, hlsl, entry = "vs_main");
+    let spirv = include_spirv!(
+        "src/shaders/vert.hlsl",
+        vert,
+        hlsl,
+        // This issue is only caused when debug is on
+        // https://github.com/gfx-rs/wgpu/issues/4532
+        no_debug,
+        entry = "vs_main"
+    );
 
     device.create_shader_module(&wgpu::ShaderModuleDescriptor {
         label: None,
@@ -11,7 +19,15 @@ pub fn vert_shader(device: &wgpu::Device) -> wgpu::ShaderModule {
 }
 
 pub fn frag_shader(device: &wgpu::Device) -> wgpu::ShaderModule {
-    let spirv = include_spirv!("src/shaders/frag.hlsl", frag, hlsl, entry = "fs_main");
+    let spirv = include_spirv!(
+        "src/shaders/frag.hlsl",
+        frag,
+        hlsl,
+        // This issue is only caused when debug is on
+        // https://github.com/gfx-rs/wgpu/issues/4532
+        no_debug,
+        entry = "fs_main"
+    );
 
     device.create_shader_module(&wgpu::ShaderModuleDescriptor {
         label: None,
